8f74ca0bf4d9de1635e62e1cb1401149ae4adeaa
TomVerkon <tverkon@gmail.com>
2025-08-03 15:54:50 -0400

    finished create & update invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,77 @@
+'use server';
+ 
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+import { z } from 'zod';
+import { CustomerField, InvoiceForm } from './definitions';
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+// Use Zod to create/update the expected types
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function fetchCustomers() {
+  const customers = await sql<CustomerField[]>`
+    SELECT id, name, email, image_url
+    FROM customers
+    ORDER BY name ASC
+  `;
+  return customers;
+}
+export async function fetchInvoiceById(id: string) {
+  const invoice = await sql<InvoiceForm[]>`
+    SELECT id, customer_id, amount, status, date
+    FROM invoices
+    WHERE id = ${id}
+  `;
+  return invoice[0]; // Return the first invoice found
+}
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  console.log('Creating invoice:', { customerId, amountInCents, status, date });
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
